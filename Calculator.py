0367ac4fda4708d795b8eb9840b7a0ec790b8d19
Seleman Hemed <shemed.blackops@gmail.com>
2020-11-27 12:44:57 +0300

    Added Calculator Module

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+import math
+
+class Calculator:
+
+    def __init__(self, operation):
+        self.operation = operation
+
+    def addition(self, *numbers):
+        return sum(numbers)
+
+    def multiplication(self, *numbers):
+        pass
+
+    
+
+        
