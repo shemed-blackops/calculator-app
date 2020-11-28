4214d1a6376d9223bfc99334da62c73c472bf388
Seleman Hemed <shemed.blackops@gmail.com>
2020-11-28 20:12:03 +0300

    Updated Calculator Class

--- Calculator.py
+++ Calculator.py
@@ -1,16 +1,22 @@
-import math
+class Car:
 
-class Calculator:
-
-    def __init__(self, operation):
-        self.operation = operation
-
-    def addition(self, *numbers):
-        return sum(numbers)
-
-    def multiplication(self, *numbers):
-        pass
-
+    isOn = False
     
+    def __init__(self, brand, model):
+        self.brand = brand
+        self.model = model
 
-        
+    def start(self):
+        if self.isOn is True:
+            print("{} {} is already On".format(self.brand, self.model))
+        else:
+            self.isOn = True
+            print("{} {} has started".format(self.brand, self.model))
+    
+    def stop(self):
+        if self.isOn is False:
+            print("{} {} is already stopped".format(self.brand, self.model))
+        else:
+            self.isOn = False
+            print("{} {} has stopped".format(self.brand, self.model))
+
---
+++ app.py
@@ -0,0 +1,6 @@
+from calculator import *
+
+toyota = Car('Toyota', 'RAV 4')
+
+toyota.start()
+toyota.stop()
